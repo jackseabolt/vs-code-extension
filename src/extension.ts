202740a8cc94b1b671ff0ff2c72d5bb4966d0107
jackseabolt <johnseabolt@Johns-MacBook-Pro.local>
2025-04-12 09:08:45 -0700

    added react app test app

--- src/extension.ts
+++ src/extension.ts
@@ -1,4 +1,6 @@
 import * as vscode from "vscode";
+import * as path from "path";
+import * as fs from "fs";
 import { SidebarWebViewProvider } from "./views/sidebarWebViewProvider";
 
 export function activate(context: vscode.ExtensionContext) {
@@ -9,6 +11,37 @@ export function activate(context: vscode.ExtensionContext) {
    */
   context.subscriptions.push(
     vscode.window.registerWebviewViewProvider("cognichip-sidebar", provider)
+  );
+
+  context.subscriptions.push(
+    vscode.commands.registerCommand("viteReactWebview.showPanel", () => {
+      const panel = vscode.window.createWebviewPanel(
+        "viteReactWebview",
+        "Vite React Webview",
+        vscode.ViewColumn.One,
+        {
+          enableScripts: true,
+        }
+      );
+
+      const htmlPath = path.join(
+        context.extensionPath,
+        "react-media",
+        "index.html"
+      );
+      let html = fs.readFileSync(htmlPath, "utf8");
+
+      // Fix the paths for JS/CSS so they load properly in the webview
+      html = html.replace(/(href|src)="(.+?)"/g, (match, p1, p2) => {
+        const fullPath = vscode.Uri.file(
+          path.join(context.extensionPath, "react-media", p2)
+        );
+        const webviewUri = panel.webview.asWebviewUri(fullPath);
+        return `${p1}="${webviewUri}"`;
+      });
+
+      panel.webview.html = html;
+    })
   );
 
   /**
---
+++ webview-ui/src/App.tsx
@@ -0,0 +1,11 @@
+import "./App.css";
+
+function App() {
+  return (
+    <>
+      <div>Hello Again again</div>
+    </>
+  );
+}
+
+export default App;
